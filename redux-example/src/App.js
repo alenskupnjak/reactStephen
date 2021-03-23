08323947eec2edef6ca356f2cf78cc55df403c7b
AlenS <alenskupnjak@yahoo.com>
2021-03-23 15:28:21 +0100

    003 react widgets

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,120 +1,125 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import './App.css';
 
 function App() {
-// ACTION ACTION ACTION
-const createPolicy = (name, amount) => {
-  return {
-    type: 'CREATE_POLICY',
-    payload: {
-      name: name,
-      amount: amount
-    }
+  // ACTION ACTION ACTION
+  const createPolicy = (name, amount) => {
+    return {
+      type: 'CREATE_POLICY',
+      payload: {
+        name: name,
+        amount: amount,
+      },
+    };
   };
-};
 
-const createUpozorenje = (name) => {
-  return {
-    type: 'CREATE_STOP',
-    payload: {
-      name: name,
-      obavijest: `${name} STOP`
-    }
+  const createUpozorenje = (name) => {
+    return {
+      type: 'CREATE_STOP',
+      payload: {
+        name: name,
+        obavijest: `${name} STOP`,
+      },
+    };
   };
-};
 
-const deletePolicy = (name) => {
-  return {
-    type: 'DELETE_POLICY',
-    payload: {
-      name: name
-    }
+  const deletePolicy = (name) => {
+    return {
+      type: 'DELETE_POLICY',
+      payload: {
+        name: name,
+      },
+    };
   };
-};
 
-const createClaim = (name, amountOfMoneyToCollect) => {
-  return {
-    type: 'CREATE_CLAIM',
-    payload: {
-      name: name,
-      amountOfMoneyToCollect: amountOfMoneyToCollect,
-      dodatak: 'evo slona'
-    }
+  const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+      type: 'CREATE_CLAIM',
+      payload: {
+        name: name,
+        amountOfMoneyToCollect: amountOfMoneyToCollect,
+        dodatak: 'evo slona',
+      },
+    };
   };
-};
+
+  // REDUCERS REDUCERS
+  const claimsHistoryReducer = (oldListOfClaims = [], action) => {
+    if (action.type === 'CREATE_CLAIM') {
+      return [...oldListOfClaims, action.payload];
+    }
+    return oldListOfClaims;
+  };
+  const policyHistoryReducer = (oldListOfPolicy = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+      return [...oldListOfPolicy, action.payload];
+    }
+    return oldListOfPolicy;
+  };
+
+  const accountingReducer = (bagOfMoney = 100, action) => {
+    if (action.type === 'CREATE_CLAIM') {
+      return bagOfMoney - action.payload.amountOfMoneyToCollect;
+    } else if (action.type === 'CREATE_POLICY') {
+      return bagOfMoney + action.payload.amount;
+    }
+    return bagOfMoney;
+  };
+
+  const policiesReducer = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+      return [...listOfPolicies, action.payload.name];
+    } else if (action.type === 'DELETE_POLICY') {
+      return listOfPolicies.filter((name) => name !== action.payload.name);
+    } else if (action.type === 'CREATE_STOP') {
+      return [
+        ...listOfPolicies.filter((name) => name !== action.payload.name),
+        action.payload.obavijest,
+      ];
+    }
+    return listOfPolicies;
+  };
+
+  const ourDepartments = combineReducers({
+    claimsHistory: claimsHistoryReducer,
+    accounting: accountingReducer,
+    policies: policiesReducer,
+    policyHistory: policyHistoryReducer,
+  });
+
+  const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        })
+      : compose;
 
 
-// REDUCERS REDUCERS
-const claimsHistoryReducer = (oldListOfClaims = [], action) => {
-  if (action.type === 'CREATE_CLAIM') {
-    return [...oldListOfClaims, action.payload];
-  }
-  return oldListOfClaims;
-};
-const policyHistoryReducer = (oldListOfPolicy = [], action) => {
-  if (action.type === 'CREATE_POLICY') {
-    return [...oldListOfPolicy, action.payload];
-  }
-  return oldListOfPolicy;
-};
+  const store = createStore(
+    ourDepartments,
+    composeEnhancers(applyMiddleware()),
+  );
 
-const accountingReducer = (bagOfMoney = 100, action) => {
-  if (action.type === 'CREATE_CLAIM') {
-    return bagOfMoney - action.payload.amountOfMoneyToCollect;
-  } else if (action.type === 'CREATE_POLICY') {
-    return bagOfMoney + action.payload.amount;
-  }
-  return bagOfMoney;
-};
+  store.dispatch(createPolicy('Alex', 20));
+  store.dispatch(createPolicy('Alex', 20));
+  store.dispatch(createPolicy('Tin', 20));
+  store.dispatch(createPolicy('Jim', 30));
+  store.dispatch(createPolicy('Bob', 40));
+  store.dispatch(createPolicy('Alen', 100));
+  store.dispatch(createPolicy('Alen', 100));
+  store.dispatch(createPolicy('Mato', 100));
+  store.dispatch(createUpozorenje('Mato'));
 
-const policiesReducer = (listOfPolicies = [], action) => {
-  if (action.type === 'CREATE_POLICY') {
-    return [...listOfPolicies, action.payload.name];
-  } else if (action.type === 'DELETE_POLICY') {
-    return listOfPolicies.filter(name => name !== action.payload.name);
-  } else if (action.type === 'CREATE_STOP') {
-    return [...listOfPolicies.filter(name => name !== action.payload.name), action.payload.obavijest];
-  }
-  return listOfPolicies;
-};
+  store.dispatch(createClaim('Alex', 120));
+  store.dispatch(createClaim('Jim', 50));
+  store.dispatch(createClaim('Alen', 20));
 
+  store.dispatch(deletePolicy('Alen'));
 
-
-const ourDepartments = combineReducers({
-  claimsHistory: claimsHistoryReducer,
-  accounting: accountingReducer,
-  policies: policiesReducer,
-  policyHistory:policyHistoryReducer
-});
-
-const store = createStore(ourDepartments);
-
-
-
-store.dispatch(createPolicy('Alex', 20));
-store.dispatch(createPolicy('Alex', 20));
-store.dispatch(createPolicy('Jim', 30));
-store.dispatch(createPolicy('Bob', 40));
-store.dispatch(createPolicy('Alen', 100));
-store.dispatch(createPolicy('Alen', 100));
-store.dispatch(createPolicy('Mato', 100));
-store.dispatch(createUpozorenje('Mato'));
-
-store.dispatch(createClaim('Alex', 120));
-store.dispatch(createClaim('Jim', 50));
-store.dispatch(createClaim('Alen', 20));
-
-store.dispatch(deletePolicy('Alen'));
-
-console.log(store.getState());
-
-
-
-
-
+  console.log(store.getState());
 
   return (
     <div className="App">
